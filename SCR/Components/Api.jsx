ee1ad7de0b5a364a039aa1385d939749e454dd40
Benjamin-PAZ-97 <benjhamin.xm@gmail.com>
2023-08-07 21:37:00 -0500

    posicionamiento tingo maria

--- SCR/Components/Api.jsx
+++ SCR/Components/Api.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import MapView from 'react-native-maps'
 import { View, StyleSheet } from 'react-native';
 
+
 const G_Maps = () => {
+    const [origin, setOrigin] = React.useState({
+        latitud: -9.298440,
+        longitud: - 76.000381,
+    })
     return (
         <View style={styles.container} >
-            <MapView style={styles.map} />
+            <MapView style={styles.map}
+                initialRegion={{
+                    latitude: origin.latitud,
+                    longitude: origin.longitud,
+                    latitudeDelta: 0.09,
+                    longitudeDelta: 0.04
+                }}
+            />
         </View>
 
     );
